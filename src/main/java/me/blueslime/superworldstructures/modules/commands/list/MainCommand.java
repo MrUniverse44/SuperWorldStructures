982757c1815120fe9a2e695227bf19735114f637
MrUniverse44 <oficial.mruniverse@gmail.com>
2024-03-15 05:24:02 -0500

    feat: find argument for command

--- src/main/java/me/blueslime/superworldstructures/modules/commands/list/MainCommand.java
+++ src/main/java/me/blueslime/superworldstructures/modules/commands/list/MainCommand.java
@@ -3,7 +3,10 @@ package me.blueslime.superworldstructures.modules.commands.list;
 import me.blueslime.superworldstructures.SuperWorldStructures;
 import me.blueslime.utilitiesapi.commands.AdvancedCommand;
 import me.blueslime.utilitiesapi.commands.sender.Sender;
+import me.blueslime.utilitiesapi.tools.PluginTools;
+import org.bukkit.entity.Player;
 
+import java.util.List;
 import java.util.Locale;
 
 public class MainCommand extends AdvancedCommand<SuperWorldStructures> {
@@ -24,7 +27,8 @@ public class MainCommand extends AdvancedCommand<SuperWorldStructures> {
         }
         if (arguments.length == 0 || arguments[0].equalsIgnoreCase("help")) {
             sender.send(
-            "&6/" + command + " reload &7- &eReload command",
+                "&6/" + command + " reload &7- &eReload command",
+                "&6/" + command + " find &7- &eFind structures in this world",
                 "&6",
                 "&f&oCreated by JustJustin. Version: " + getPlugin().getDescription().getVersion()
             );
@@ -40,6 +44,47 @@ public class MainCommand extends AdvancedCommand<SuperWorldStructures> {
                 "&aPlugin has been reloaded successfully."
             );
 
+            return;
+        }
+
+        if (argument.equals("find") && sender.isPlayer()) {
+            Player player = sender.toPlayer();
+
+            String worldName = player.getWorld().getName();
+
+            if (!getPlugin().getSettings().contains("spawned-structures." + worldName)) {
+                sender.send("&cNo structures found in this world.");
+                return;
+            }
+
+            sender.send("&6Showing structures for world: &f" + worldName);
+
+            List<String> structures = getPlugin().getSettings().getStringList("spawned-structures." + worldName);
+
+            for (String location : structures) {
+                String[] split = location.replace(" ", "").split(";");
+
+                String id = split[0].replace("Structure:", "")
+                        .replace(" ", "");
+
+                String defValues = split.length >= 2 ? split[1] : "x=0y=0z=0";
+
+                defValues = defValues.replace("x=", "")
+                        .replace("y=", ",")
+                        .replace("z=", ",")
+                        .replace(" ", "");
+
+                String[] splitValues = defValues.split(",");
+
+                int x = splitValues.length >= 1 && !splitValues[0].isEmpty() ? PluginTools.isNumber(splitValues[0]) ? Integer.parseInt(splitValues[0]) : 0 : 0;
+                int y = splitValues.length >= 2 && !splitValues[1].isEmpty() ? PluginTools.isNumber(splitValues[1]) ? Integer.parseInt(splitValues[1]) : 0 : 0;
+                int z = splitValues.length >= 3 && !splitValues[1].isEmpty() ? PluginTools.isNumber(splitValues[2]) ? Integer.parseInt(splitValues[2]) : 0 : 0;
+
+                sender.send(
+                    "  &eStructure ID: &7" + id + " &ex: &b" + x + "&e, y:&b " + y + "&e, z:&b " + z
+                );
+            }
+            sender.send("&6The plugin found &a" + structures.size() + "&6 structures in this world.");
             return;
         }
 
